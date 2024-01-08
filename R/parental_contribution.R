b7d6abf1f8136a68c9b47ac8bda52a3a61876d66
VITALE <p.vitale@cimmyt.org>
2024-01-08 11:52:13 -0600

    v 6

--- R/parental_contribution.R
+++ R/parental_contribution.R
@@ -9,14 +9,14 @@
 #'
 #' @examples parental_contribution(results_df)
 parental_contribution <- function(result_df) {
-  result_df$similarity_son_parent1 <- result_df$Son_Parent1_Common / result_df$Son_Parent1_total
-  result_df$similarity_son_parent2 <- result_df$Son_Parent2_Common / result_df$Son_Parent2_total
+  result_df$similarity_offspring_parent1 <- result_df$offspring_Parent1_Common / result_df$offspring_Parent1_total
+  result_df$similarity_offspring_parent2 <- result_df$offspring_Parent2_Common / result_df$offspring_Parent2_total
   result_df$similarity_parent1_parent2 <-  result_df$parent1_parent2_common / result_df$Parent1_Parent2_total
 
-  result_df$contribution_parent1 <- (result_df$similarity_son_parent1 - (result_df$similarity_son_parent2 * result_df$similarity_parent1_parent2)) /
+  result_df$contribution_parent1 <- (result_df$similarity_offspring_parent1 - (result_df$similarity_offspring_parent2 * result_df$similarity_parent1_parent2)) /
     (1 - (result_df$similarity_parent1_parent2 * result_df$similarity_parent1_parent2))
 
-  result_df$contribution_parent2 <- (result_df$similarity_son_parent2 - (result_df$similarity_son_parent1 * result_df$similarity_parent1_parent2)) /
+  result_df$contribution_parent2 <- (result_df$similarity_offspring_parent2 - (result_df$similarity_offspring_parent1 * result_df$similarity_parent1_parent2)) /
     (1 - (result_df$similarity_parent1_parent2 * result_df$similarity_parent1_parent2))
 
   result_df$contribution_sum <- result_df$contribution_parent1 + result_df$contribution_parent2
